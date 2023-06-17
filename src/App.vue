a6842e9112207454bdfa38e2a950feccfba76389
grenzk <74033991+grenzk@users.noreply.github.com>
2023-06-17 15:12:01 +0800

    Structure html

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <script setup></script>
 
 <template>
-  <h1>Hello, Vue!</h1>
+  <main>
+    <div class="card">
+      <img src="./assets/img/image-product-desktop.jpg" alt="Chanel Perfume" />
+
+      <div class="product-info">
+        <p class="category">Perfume</p>
+
+        <h1 class="title">Gabrielle Essence Eau De Parfum</h1>
+        <p>
+          A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator
+          for the House of CHANEL.
+        </p>
+
+        <div class="price">
+          <p class="discount">$149.99</p>
+          <p class="original">$169.99</p>
+        </div>
+
+        <button class="button">
+          <img src="./assets/img/icon-cart.svg" />
+          <small>Add to Cart</small>
+        </button>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss"></style>

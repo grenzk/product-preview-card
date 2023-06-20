f27f8ae6f9d0c01cedb6000c4e17a3a59c4d0cd0
grenzk <74033991+grenzk@users.noreply.github.com>
2023-06-20 11:02:30 +0800

    Add style to product info and make it responsive

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,16 @@
     <div class="card">
       <img class="product-img" src="./assets/img/image-product-desktop.jpg" alt="Chanel Perfume" />
 
-      <div class="product-info">
+      <div class="product-info l-flex">
         <p class="category">Perfume</p>
 
         <h1 class="title">Gabrielle Essence Eau De Parfum</h1>
-        <p>
+        <p class="description">
           A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator
           for the House of CHANEL.
         </p>
 
-        <div class="price">
+        <div class="price l-inline">
           <p class="discount">$149.99</p>
           <p class="original">$169.99</p>
         </div>
@@ -37,7 +37,7 @@
   display: flex;
   flex-direction: column;
   max-width: 46rem;
-  margin: 2rem 1rem;
+  margin: 0 1rem;
 }
 
 .product-img {
@@ -45,5 +45,59 @@
   object-fit: cover;
   aspect-ratio: 3 / 2.35;
   border-radius: 1rem 1rem 0 0;
+}
+
+.product-info {
+  background-color: hsl(var(--neutral-white));
+  border-radius: 0 0 1rem 1rem;
+
+  .category {
+    text-transform: uppercase;
+    letter-spacing: 0.4rem;
+  }
+
+  .title {
+    font-family: var(--heading-font);
+    font-size: var(--biggest-font-size);
+    color: hsl(var(--neutral-dark-blue));
+    line-height: 1;
+  }
+
+  .description {
+    font-size: var(--h3-font-size);
+    line-height: 1.6;
+  }
+
+  .price {
+    width: 67%;
+
+    .discount {
+      font-family: var(--heading-font);
+      font-size: var(--biggest-font-size);
+      color: hsl(var(--primary-dark-cyan));
+    }
+
+    .original {
+      font-size: var(--h3-font-size);
+      text-decoration: line-through;
+    }
+  }
+
+  .button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    column-gap: 1rem;
+    padding: 1rem;
+    color: hsl(var(--neutral-white));
+    font-size: var(--h3-font-size);
+    background-color: hsl(var(--primary-dark-cyan));
+    border: none;
+    border-radius: 0.6rem;
+
+    img {
+      width: 6%;
+    }
+  }
 }
 </style>
